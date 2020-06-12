3c4e418e40bdd107f0edb76d3b9955f3b51d74e6
Peter Liu <peter.ty.liu@gmail.com>
2020-06-11 23:16:12 -0400

    Added readme, updated workout schema to validate exercises and provide totalDuration property

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# thirteenth-assignment-workout-tracker
-Assignment 13: Workout Tracker
+# Assignment 13: Fitness Tracker
+
+## Web app to track workouts and visualize weekly exercise patterns, built using Mongoose and Node
+
+Home page displys statistics of the previous workout: the user can continue that workout or create a new one.
+
+![home](images/home.png)
+
+Users can add two types of exercise: cardio or resistance. Each type of exercise has different properties that the user can enter.
+
+![add an exercise](images/exercise.png)
+
+The user has access to a dashboard which displays the statistics of the last week of exercise.
+
+![dashboard](images/dashboard.png)
+
+## Live site: https://immense-atoll-66797.herokuapp.com/
--- models/Workout.js
+++ models/Workout.js
@@ -1,9 +1,56 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: { type: Date, default: Date.now },
-  exercises: [],
+const workoutSchema = new Schema(
+  {
+    day: { type: Date, default: Date.now },
+    exercises: [
+      {
+        name: {
+          type: String,
+          trim: true,
+          required: "Name is Required",
+        },
+
+        type: {
+          type: String,
+          trim: true,
+          required: "Type is Required",
+        },
+
+        duration: {
+          type: Number,
+          required: "duration is required",
+        },
+
+        weight: {
+          type: Number,
+        },
+
+        reps: {
+          type: Number,
+        },
+
+        sets: {
+          type: Number,
+        },
+
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce(
+    (total, exercise) => total + exercise.duration,
+    0
+  );
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
